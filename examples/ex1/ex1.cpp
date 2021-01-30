190f66cdbb8ae00accf90073b6584777516b8d7a
Jeff Webb <jeff.webb@codecraftsmen.org>
2021-01-29 18:02:38 -0600

    Improved ex1.cpp

--- examples/ex1/ex1.cpp
+++ examples/ex1/ex1.cpp
@@ -7,29 +7,57 @@ int main()
   using namespace std;
   
   Child c;
-  cout << sizeof(c) << endl;
   c.member1(1)
    .member2(2);
-  cout << c.member1() << " " << c.member2() << endl;
+  
+  cout << "Child" << endl;
+  cout << "  size: " << sizeof(c) << " bytes" << endl;
+  cout << "  member1: " << c.member1() << endl;
+  cout << "  member2: " << c.member2() << endl;
   
   Parent p;
   p.child1.member1(3)
           .member2(4)
-          .member2(44)
-          .member1(33);
+          .member2(44)  // reassign
+          .member1(33); // reassign
   p.child2.member1(5)
           .member2(6);
-  p.child2(c);
-  
-  cout << p.child1().member1() << " " << p.child1().member2() << endl;
-  cout << p.child2().member1() << " " << p.child2().member2() << endl;
 
+  cout << endl;
+  cout << "Parent" << endl;
+  cout << "  size: " << sizeof(p) << " bytes" << endl;
+  cout << "  child1" << endl;
+  cout << "    member1: " << p.child1.member1() << endl;
+  cout << "    member2: " << p.child1.member2() << endl;
+  cout << "  child2" << endl;
+  cout << "    member1: " << p.child2.member1() << endl;
+  cout << "    member2: " << p.child2.member2() << endl;
+
+  p.child2(c); // call setter using another instance of the type
+
+  // Note that parentheses on the child accessor also works
+  cout << endl;
+  cout << "Parent" << endl;
+  cout << "  child1" << endl;
+  cout << "    member1: " << p.child1().member1() << endl;
+  cout << "    member2: " << p.child1().member2() << endl;
+  cout << "  child2" << endl;
+  cout << "    member1: " << p.child2().member1() << endl;
+  cout << "    member2: " << p.child2().member2() << endl;
+
+  // Use assignment operator
+  p.child2 = p.child1(); // Need parenthesis here
   p.child1 = c;
   p.child2.member1 = 5;
   
-  // This form works also
-  cout << p.child1.member1() << " " << p.child1.member2() << endl;
-  cout << p.child2.member1() << " " << p.child2.member2() << endl;
+  cout << endl;
+  cout << "Parent" << endl;
+  cout << "  child1" << endl;
+  cout << "    member1: " << p.child1.member1() << endl;
+  cout << "    member2: " << p.child1.member2() << endl;
+  cout << "  child2" << endl;
+  cout << "    member1: " << p.child2.member1() << endl;
+  cout << "    member2: " << p.child2.member2() << endl;
 
   return 0;
 }
